80353fcfc8ec3c58155b362bd4a0b2f67753cb81
khalid7216 <khalidsanawer2@gmail.com>
2025-12-22 02:28:16 +0500

    push

--- bullet_point.js
+++ bullet_point.js
@@ -11,8 +11,8 @@ if(exp){
 }
 function UpdateBullet(expID, bulletIndex,NewText) {
     const exp =resumeData.experience.find(e.id === expID);
-    if(exp && exp.bullet[bulletIndex])
-        {
-
-    }
+    if(exp && exp.bullet[bulletIndex]!==undefined)
+        { exp.bullets[bulletIndex]= NewText;
+            return true}
+    return false
 }
