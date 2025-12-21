4898d161112fd8181b2f575c8863a5a2ddef63d4
khalid7216 <khalidsanawer2@gmail.com>
2025-12-21 19:55:58 +0500

    push

--- bullet_point.js
+++ bullet_point.js
@@ -3,4 +3,16 @@ function getAllExperience() {
 }
 function AddBullPoint(expId, bulletsText){
 const exp = resumeData.experience.find(e => e.id=== expId)
+if(exp){
+    exp.bullet.push(bulletsText);
+   return true;
+}
+ return  false;
+}
+function UpdateBullet(expID, bulletIndex,NewText) {
+    const exp =resumeData.experience.find(e.id === expID);
+    if(exp && exp.bullet[bulletIndex])
+        {
+
+    }
 }
