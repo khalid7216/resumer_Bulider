8bdb5bf303e25b5cebcfc594ace65eac57a04748
khalid7216 <khalidsanawer2@gmail.com>
2025-12-22 16:51:25 +0500

    push

--- bullet_point.js
+++ bullet_point.js
@@ -16,3 +16,46 @@ function UpdateBullet(expID, bulletIndex,NewText) {
             return true}
     return false
 }
+function DeleteBullet(expId,bulletIndex) {
+      const exp = resumeData.experience.find(e => e.id === expId);
+  if (exp && exp.bullets[bulletIndex] !== undefined) {
+    exp.bullets.splice(bulletIndex, 1);
+    return true;
+  }
+  return false;
+}
+function addEducation(degree, school, year, gpa, description) {
+  const newEdu = {
+    id: Date.now(),
+    degree: degree || '',
+    school: school || '',
+    year: year || '',
+    gpa: gpa || '',
+    description: description || ''
+  };
+  
+  resumeData.education.push(newEdu);
+  return newEdu;
+}
+
+function updateEducation(id, field, value) {
+  const edu = resumeData.education.find(e => e.id === id);
+  if (edu && edu.hasOwnProperty(field)) {
+    edu[field] = value;
+    return true;
+  }
+  return false;
+}
+
+function deleteEducation(id) {
+  const index = resumeData.education.findIndex(e => e.id === id);
+  if (index > -1) {
+    resumeData.education.splice(index, 1);
+    return true;
+  }
+  return false;
+}
+
+function getAllEducation() {
+  return resumeData.education;
+}
